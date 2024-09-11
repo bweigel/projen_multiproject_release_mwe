b92b628cf1cbb4737c89d8a65592dac8fd953d3c
Benjamin Genz <github@dreigelb.eu>
2024-09-11 17:27:30 +0200

    initial commit

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,7 +1,35 @@
 import { monorepo } from "@aws/pdk";
+import { AwsCdkConstructLibrary } from "projen/lib/awscdk";
+
 const project = new monorepo.MonorepoTsProject({
   devDeps: ["@aws/pdk"],
   name: "projen_multiproject_release_mwe",
   projenrcTs: true,
 });
+
+new AwsCdkConstructLibrary({
+  parent: project,
+  author: "author",
+  authorAddress: "email@mail.com",
+  cdkVersion: "2.157.0",
+  defaultReleaseBranch: "main",
+  name: "package_a",
+  release: true,
+  repositoryUrl: "https://repository.url",
+  outdir: "packages/package_a",
+});
+
+
+new AwsCdkConstructLibrary({
+  parent: project,
+  author: "author",
+  authorAddress: "email@mail.com",
+  cdkVersion: "2.157.0",
+  defaultReleaseBranch: "main",
+  name: "package_b",
+  release: true,
+  repositoryUrl: "https://repository.url",
+  outdir: "packages/package_b",
+});
+
 project.synth();
